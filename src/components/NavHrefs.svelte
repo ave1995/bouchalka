a2a9477887285b4227531c32e33de11d23f300e6
ave <veselya95@gmail.com>
2024-07-14 15:02:17 +0200

    menu

---
+++ src/components/NavHrefs.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let opened: boolean;
+</script>
+
+<a href="/about" on:click={() => (opened = false)}>O nás</a>
+<a href="/dogs" on:click={() => (opened = false)}>Psi</a>
+<a href="/sponsors" on:click={() => (opened = false)}>Sponzoři</a>
+<a href="/contact" on:click={() => (opened = false)}>Kontakt</a>
+<a href="/donate" class="hover:animate-pulse" on:click={() => (opened = false)}>
+  <img src="/helpdog.png" class="w-7 h-7" alt="Bouchalka" />
+</a>
+
+<style>
+  a:hover {
+    color: black;
+  }
+  a {
+    --tw-text-opacity: 1;
+    color: rgb(107 114 128 / var(--tw-text-opacity)) /* #6b7280 */;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,46 @@
 <script>
   import "../app.css";
+  import NavHrefs from "../components/NavHrefs.svelte";
+  import { slide } from "svelte/transition";
+  let opened = false;
 </script>
 
-<div class="sm:max-w-[640px] md:max-w-[768px] lg:max-w-[950px] m-auto">
-  <div class="flex flex-row items-center py-3 place-content-between">
-    <a href="/" class="flex flex-row items-end gap-3">
-      <img src="/housedog.png" class="w-11 h-11" alt="Bouchalka" />
-      <h1 class="text-3xl font-semibold">Bouchalka</h1>
+<div
+  class="sm:max-w-[640px] md:max-w-[768px] lg:max-w-[950px] m-auto px-3 sm:px-3 md:px-0 lg:px-0 flex flex-col"
+>
+  {#if opened}
+    <aside
+      class="flex flex-col md:hidden lg:hidden place-items-center gap-2 {opened
+        ? 'border-b pb-2'
+        : ''}"
+      transition:slide
+    >
+      <NavHrefs bind:opened={opened}></NavHrefs>
+    </aside>
+  {/if}
+  <button
+    on:click={() => (opened = !opened)}
+    class="text-gray-500 pt-2 flex grow self-center md:hidden lg:hidden"
+    >Menu</button
+  >
+
+  <div
+    class="flex flex-row py-3 place-content-center md:place-content-between lg:place-content-between"
+  >
+    <a href="/" class="flex flex-row gap-3 items-end">
+      <img
+        src="/housedog.png"
+        class="w-9 h-9 sm:w-9 sm:h-9 md:w-11 md:h-11 lg:w-11 lg:h-11"
+        alt="Bouchalka"
+      />
+      <h1 class="text-2xl font-semibold md:text-3xl lg:text-3xl">Bouchalka</h1>
     </a>
-    <nav class="flex flex-row gap-4 text-gray-500 items-center text-sm">
-      <a href="/about">O nás</a>
-      <a href="/dogs">Psi</a>
-      <a href="/sponsors">Sponzoři</a>
-      <a href="/contact">Kontakt</a>
-      <a href="/donate" class="hover:animate-pulse">
-        <img src="/helpdog.png" class="w-7 h-7" alt="Bouchalka" />
-      </a>
+    <nav
+      class="flex-row gap-4 text-gray-500 items-center text-sm hidden md:flex lg:flex"
+    >
+      <NavHrefs bind:opened={opened}></NavHrefs>
     </nav>
   </div>
 
   <slot></slot>
 </div>
-
-<style>
-    nav a:hover {
-      color: black;
-    }
-  </style>
